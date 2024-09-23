683eacbc031eb883ebf7a95644dceb23d2920e82
AvhTanuja12 <159618929+AvhTanuja12@users.noreply.github.com>
2024-09-23 18:43:48 +0530

    Add files via upload

---
+++ slip4.java
@@ -0,0 +1,10 @@
+public class slip4 {
+    public static void main (String[]args)
+    {
+        String s="Hello World!";
+        for(int i=0;i<s.length();i+=2)
+        {
+            System.out.println(s.charAt(i));
+        }
+    } 
+}
